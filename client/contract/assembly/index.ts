a06fa628e629af587d46696215740d9c70b6b4df
Rishabhraghwendra18 <rishabhraghwendra2002@gmail.com>
2021-09-18 11:49:27 +0530

    reverted back smart contract

--- client/contract/assembly/index.ts
+++ client/contract/assembly/index.ts
@@ -1,54 +1,40 @@
-import { logging, PersistentMap, PersistentVector } from "near-sdk-as";
-import { DayFees, Certificates } from "./model";
+/*
+ * This is an example of an AssemblyScript smart contract with two simple,
+ * symmetric functions:
+ *
+ * 1. setGreeting: accepts a greeting, such as "howdy", and records it for the
+ *    user (account_id) who sent the request
+ * 2. getGreeting: accepts an account_id and returns the greeting saved for it,
+ *    defaulting to "Hello"
+ *
+ * Learn more about writing NEAR smart contracts with AssemblyScript:
+ * https://docs.near.org/docs/develop/contracts/as/intro
+ *
+ */
 
-@nearBindgen
-export class Contract {
-  studentData: PersistentMap<string, PersistentVector<DayFees>>;
-  studentCertificates: PersistentMap<
-    string,
-    PersistentVector<Certificates | null>
-  >;
+import { Context, logging, storage } from 'near-sdk-as'
 
-  getStudentPayFeesDate(studentID: string): DayFees | null {
-    return this.studentData.get(studentID)![0];
-  }
-  getStudentCertificate(
-    studentID: string
-  ): PersistentVector<Certificates | null> | null {
-    return this.studentCertificates.get(studentID);
-  }
+const DEFAULT_MESSAGE = 'Hello'
 
-  @mutateState()
-  createStudentID(studentID: string, dayFees: DayFees[]): bool {
-    try {
-      const data = new PersistentVector<DayFees>(studentID);
-      for (let index = 0; index < dayFees.length; index++) {
-        data.push(dayFees[index]);
-      }
-      this.studentData.set(studentID, data);
-      this.setStudentCrtificateAsNull(studentID);
-      return true;
-    } catch (error) {
-      logging.log(`error while creating studentID in contract ${error}`);
-      return false;
-    }
-  }
-  @mutateState()
-  uploadStudentCertificate(studentID: string, certificate: Certificates): bool {
-    try {
-      const data = this.studentCertificates.get(studentID);
-      data?.push(certificate);
-      this.studentCertificates.set(studentID, data!);
-      return true;
-    } catch (error) {
-      logging.log(`error while uploading certificate in contract ${error}`);
-      return false;
-    }
-  }
-  @mutateState()
-  setStudentCrtificateAsNull(studentID: string) {
-    const temp = new PersistentVector<Certificates | null>(studentID);
-    temp.push(null);
-    this.studentCertificates.set(studentID, temp);
-  }
+// Exported functions will be part of the public interface for your smart contract.
+// Feel free to extract behavior to non-exported functions!
+export function getGreeting(accountId: string): string | null {
+  // This uses raw `storage.get`, a low-level way to interact with on-chain
+  // storage for simple contracts.
+  // If you have something more complex, check out persistent collections:
+  // https://docs.near.org/docs/concepts/data-storage#assemblyscript-collection-types
+  return storage.get<string>(accountId, DEFAULT_MESSAGE)
+}
+
+export function setGreeting(message: string): void {
+  const account_id = Context.sender
+
+  // Use logging.log to record logs permanently to the blockchain!
+  logging.log(
+    // String interpolation (`like ${this}`) is a work in progress:
+    // https://github.com/AssemblyScript/assemblyscript/pull/1115
+    'Saving greeting "' + message + '" for account "' + account_id + '"'
+  )
+
+  storage.set(account_id, message)
 }
