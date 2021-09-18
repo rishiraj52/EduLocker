dee3944c6b19d057b727d575240aabebad1032ca
Anubha13kumari <anubhamuz@gmail.com>
2021-09-18 12:07:21 +0530

    wrote js code for create-new account.js page

--- client/src/js/upload-certificate.js
+++ client/src/js/upload-certificate.js
@@ -3,14 +3,14 @@ document.getElementById("Sign-Out").addEventListener('click',(e)=>{
     e.preventDefault();
     logout();
 })
-// document.getElementById("upload-button").addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     const studentUsername=document.getElementById("username").Value;
-// })
+document.getElementById("upload-button").addEventListener('click',(e)=>{
+    e.preventDefault();
+    const studentUsername=document.getElementById("username").Value;
+})
 
 
-// document.getElementById("cancel-button" ).addEventListener('click',(e)=>{
-//     e.preventDefault();
-//     const studentUsername=document.getElementById("username").Value;
-//     studentUsername.innerHTML='';
-// })
+document.getElementById("cancel-button" ).addEventListener('click',(e)=>{
+    e.preventDefault();
+    const studentUsername=document.getElementById("username").Value;
+    studentUsername.innerHTML='';
+})
